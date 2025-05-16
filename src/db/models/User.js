0ebc6dc9eb2a656077f3ac5325c50b60fc2e21e1
Anastasia_S <sivokonanastasia1@gmail.com>
2025-05-16 21:30:44 +0300

    connect db and create model

---
+++ src/db/models/User.js
@@ -0,0 +1,34 @@
+const usersSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    },
+);
+
+
+usersSchema.methods.toJSON = function() {
+    const obj = this.toObject();
+    delete obj.password;
+    return obj;
+};
+
+
+const UsersCollection = model('users', usersSchema);
+
+
+export default UsersCollection;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import { initMongoDB } from './db/initMongoDB.js';
 import { setupServer } from './server.js';
 
+
 const bootstrap = async () => {
+  await initMongoDB();
   setupServer();
 };
+
 
 bootstrap();
