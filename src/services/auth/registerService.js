1a398777aab7bcf9f7a732c2003098cb9c1b3343
Olena <alchimeraa@gmail.com>
2025-05-17 11:00:08 +0300

    реалізація endpoint реєстрації

---
+++ src/services/auth/registerService.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs';
+import User from '../../db/models/User.js';
+
+export const registerUser = async ({ name, email, password }) => {
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new Error('Email already in use');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  return {
+    id: newUser._id,
+    name: newUser.name,
+    email: newUser.email,
+  };
+};
