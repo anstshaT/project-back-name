db3b08541f398ebb528a7d91501c6867ab2b693e
Olha <irhizovaolha@gmail.com>
2025-05-17 11:01:38 +0300

    add logger,change index.js/server.js,env.example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { initMongoDB } from './db/initMongoDB.js';
 import { setupServer } from './server.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+import 'dotenv/config';
+
 
 
 const bootstrap = async () => {
   await initMongoDB();
-  setupServer();
+  const app = setupServer();
+  const port = Number(getEnvVar("PORT", 3000));
+  app.listen(port, ()=> console.log(`Server running on ${port} port`));
+
 };
 
 
--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-export const setupServer = () => {};
+import express from "express";
+import cors from "cors";
+
+
+import { logger } from "./middlewares/logger.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
+
+export const setupServer = () => {
+    const app = express();
+
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(logger);
+
+    app.get("/auth");
+
+    app.use(notFoundHandler);
+    app.use(errorHandler);
+
+    return app;
+};
