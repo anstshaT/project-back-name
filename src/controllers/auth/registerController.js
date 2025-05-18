f2a8f030dff57cdf2c3ab0e4e95c56836845c711
Olena <alchimeraa@gmail.com>
2025-05-18 03:02:46 +0300

    убрала з middlewares файл registerValidateBody і добавила месседж при вдалій реєстрації

--- src/controllers/auth/registerController.js
+++ src/controllers/auth/registerController.js
@@ -3,7 +3,10 @@ import { registerUser } from '../../services/auth/registerService.js';
 export const registerController = async (req, res, next) => {
   try {
     const user = await registerUser(req.body);
-    res.status(201).json({ user });
+    res.status(201).json({
+      message: 'User registered successfully',
+      user,
+    });
   } catch (error) {
     if (error.message === 'Email already in use') {
       return res.status(409).json({ message: error.message });
