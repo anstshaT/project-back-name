416bb5e0d9c878a89c0f2d2043706b33c8ef1ed0
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-05-19 23:02:06 +0200

    Add documentation for login, logout, session, register and basic for user router

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import router from './routers/index.js';
 
-/* import { swaggerDocs } from './middlewares/swaggerDocs.js'; */
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
   const app = express();
@@ -21,7 +21,7 @@ export const setupServer = () => {
 
   app.get('/auth');
 
-  /*  app.use('/api-docs', swaggerDocs()); */
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
   app.use(errorHandler);
