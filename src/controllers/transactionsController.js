eb467d07feabb2642bcddb0e50b91d3e422b46e2
AskAfter <askafter904@gmail.com>
2025-05-23 12:27:33 +0200

    delete and patch routes

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,5 +1,6 @@
 import createHttpError from 'http-errors';
 import { getSummaryByPeriod } from '../services/summary.js';
+import { TransactionsCollection } from '../db/models/transaction.js';
 
 export const getSummaryController = async (req, res, next) => {
   const { period } = req.params;
@@ -26,5 +27,80 @@ export const getSummaryController = async (req, res, next) => {
     console.log(error);
 
     next(createHttpError(500, 'Server error'));
+  }
+};
+
+export const updateTransactionController = async (req, res, next) => {
+  const { id } = req.params;
+  const { transactionType, categoryId, summ, date, comment } = req.body;
+
+  if (!req.user || !req.user._id) {
+    return next(createHttpError(401, 'Unauthorized: user not authenticated'));
+  }
+
+  const userId = req.user._id;
+
+  try {
+    const transaction = await TransactionsCollection.findOne({
+      _id: id,
+      userId,
+    });
+    if (!transaction) {
+      return next(createHttpError(404, 'Transaction not found'));
+    }
+    const updatedFields = {};
+    if (transactionType) updatedFields.transactionType = transactionType;
+    if (categoryId) updatedFields.categoryId = categoryId;
+    if (summ) updatedFields.summ = summ;
+    if (date) updatedFields.date = date;
+    if (comment) updatedFields.comment = comment;
+
+    const updatedTransaction = await TransactionsCollection.findByIdAndUpdate(
+      id,
+      updatedFields,
+      { new: true },
+    );
+
+    if (!updatedTransaction) {
+      return next(createHttpError(404, 'Transaction not found'));
+    }
+
+    res.json({
+      status: 200,
+      message: 'Transaction updated successfully',
+      transaction: updatedTransaction,
+    });
+  } catch (error) {
+    console.log(error);
+    return next(createHttpError(500, 'Server error'));
+  }
+};
+
+export const deleteTransactionController = async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!req.user || !req.user._id) {
+    return next(createHttpError(401, 'Unauthorized: user not authenticated'));
+  }
+
+  const userId = req.user._id;
+
+  try {
+    const transaction = await TransactionsCollection.findOneAndDelete({
+      _id: id,
+      userId,
+    });
+
+    if (!transaction) {
+      return next(createHttpError(404, 'Transaction not found'));
+    }
+
+    res.json({
+      status: 200,
+      message: 'Transaction deleted successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    return next(createHttpError(500, 'Server error'));
   }
 };
---
+++ src/validation/transaction/updateTransaction.js
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+import mongoose from 'mongoose';
+
+const validateObjectId = (value, helpers) => {
+  if (!mongoose.Types.ObjectId.isValid(value)) {
+    return helpers.error('any.invalid');
+  }
+  return value;
+};
+
+export const updateTransactionSchema = Joi.object({
+  transactionType: Joi.string().valid('income', 'expense'),
+  categoryId: Joi.string().custom(validateObjectId, 'valid MongoDB id'),
+  summ: Joi.number().min(0),
+  date: Joi.date(),
+  comment: Joi.string().max(300).allow('', null),
+});
