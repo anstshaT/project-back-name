56dd64b500f175182040deedc9f55b2ffa7e522d
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-05-26 14:11:30 +0200

    Add chahes

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,4 +1,4 @@
-import createHttpError from 'http-errors';
+/* import createHttpError from 'http-errors';
 import { SessionCollection } from '../db/models/Session.js';
 import UsersCollection from '../db/models/User.js';
 
@@ -41,5 +41,49 @@ export const authenticate = async (req, res, next) => {
     next();
   } catch (err) {
     next(err);
+  }
+}; */
+
+import createHttpError from 'http-errors';
+import { SessionCollection } from '../db/models/Session.js';
+import UsersCollection from '../db/models/User.js';
+
+export const authenticate = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization || '';
+    let session = null;
+
+    if (authHeader.startsWith('Bearer ')) {
+      const accessToken = authHeader.replace('Bearer ', '').trim();
+      console.log('Access Token:', accessToken);
+
+      session = await SessionCollection.findOne({ accessToken });
+
+      if (!session) {
+        throw createHttpError(
+          401,
+          'Invalid or expired token (session not found)',
+        );
+      }
+
+      if (session.accessTokenValidUntil < new Date()) {
+        throw createHttpError(401, 'Access token expired');
+      }
+
+      const user = await UsersCollection.findById(session.userId).select(
+        '-password',
+      );
+
+      if (!user) {
+        throw createHttpError(401, 'User not found');
+      }
+
+      req.user = user;
+      next();
+    } else {
+      throw createHttpError(401, 'No access token provided');
+    }
+  } catch (error) {
+    next(error);
   }
 };
