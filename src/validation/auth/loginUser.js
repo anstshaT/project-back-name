94035015fcf5d1a8559dced127faaad25a02a8b7
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-06-01 20:00:30 +0200

    Add full validation for email

--- src/validation/auth/loginUser.js
+++ src/validation/auth/loginUser.js
@@ -1,6 +1,19 @@
 import Joi from 'joi';
 
+const domains = ['.com', '.net', '.org'];
+
 export const loginUserSchema = Joi.object({
-  email: Joi.string().email().max(64).required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .max(64)
+    .required()
+    .custom((value, helpers) => {
+      if (!domains.some((domain) => value.endsWith(domain))) {
+        return helpers.error('any.invalid', {
+          message: `The domain must be one of: ${domains.join(', ')}`,
+        });
+      }
+      return value;
+    }, 'Checking allowed domains'),
   password: Joi.string().min(8).max(64).required(),
 });
--- src/validation/auth/registerValidation.js
+++ src/validation/auth/registerValidation.js
@@ -1,7 +1,20 @@
 import Joi from 'joi';
 
+const domains = ['.com', '.net', '.org'];
+
 export const registerValidationSchema = Joi.object({
   name: Joi.string().min(2).max(32).required(),
-  email: Joi.string().email().max(64).required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .max(64)
+    .required()
+    .custom((value, helpers) => {
+      if (!domains.some((domain) => value.endsWith(domain))) {
+        return helpers.error('any.invalid', {
+          message: `The domain must be one of: ${domains.join(', ')}`,
+        });
+      }
+      return value;
+    }, 'Checking allowed domains'),
   password: Joi.string().min(8).max(64).required(),
 });
